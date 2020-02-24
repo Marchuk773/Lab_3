d4f9bff467d2634903a2cdb8a65703631fcd8d6c
Roman <marchukroman228@gmail.com>
2020-02-24 10:29:46 +0200

    added 4 lab

--- src/test/java/ua/lviv/iot/tools/manager/ToolsManagerUtilsTest.java
+++ src/test/java/ua/lviv/iot/tools/manager/ToolsManagerUtilsTest.java
@@ -1,7 +1,6 @@
 package ua.lviv.iot.tools.manager;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 
 public class ToolsManagerUtilsTest extends BaseToolsManagerTest {
